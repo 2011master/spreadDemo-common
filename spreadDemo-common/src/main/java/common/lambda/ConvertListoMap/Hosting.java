1a28b52eff66c1189a7b7e2e4c046eaf63fa8498
kangguowei <kangguowei@jd.com>
2019-09-24 09:32:11 +0800

    guava bimap

--- spreadDemo-common/src/main/java/common/lambda/ConvertListoMap/Hosting.java
+++ spreadDemo-common/src/main/java/common/lambda/ConvertListoMap/Hosting.java
@@ -1,6 +1,10 @@
 package common.lambda.ConvertListoMap;
 
+import com.google.common.collect.Lists;
 import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 public class Hosting {
@@ -9,11 +13,28 @@ public class Hosting {
     private String name;
     private long websites;
 
+    public Hosting() {
+    }
+
     public Hosting(int id, String name, long websites) {
         Id = id;
         this.name = name;
         this.websites = websites;
     }
 
+
+
+
+    public static void main(String[] args) {
+
+        List<Hosting> hostings = Lists.newArrayList();
+        hostings.add(new Hosting(2, "test01", 123L));
+        Hosting reduce = hostings.stream().reduce(new Hosting(), (v1, v2) -> {
+            int sum = v1.Id + v2.Id;
+            v1.setId(sum);
+            return v1;
+        });
+        System.out.println(reduce.toString());
+    }
 
 }
---
+++ spreadDemo-common/src/main/java/guava/GuavaBiMap.java
@@ -0,0 +1,44 @@
+package guava;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+
+/**
+* @desc   bimap 双向map测试，能根据键获取值，也能根据值获取键
+* @author kanggw
+* @datetime 2019/9/24,9:28 上午
+*/
+public class GuavaBiMap {
+    /**
+     * 我们知道Map是一种键值对映射，这个映射是键到值的映射，而BiMap首先也是一种Map，他的特别之处在于，既提供键到值的映射，也提供值到键的映射，所以它是双向Map.
+     *
+     * 想象这么一个场景，我们需要做一个星期几的中英文表示的相互映射，例如Monday对应的中文表示是星期一，同样星期一对应的英文表示是Monday。这是一个绝好的使用BiMap的场景。
+     *
+     * BiMap的值键对的Map可以通过inverse()方法得到。
+     *
+     * BiMap的常用实现有：
+     *
+     * HashBiMap: key 集合与 value 集合都有 HashMap 实现
+     * EnumBiMap: key 与 value 都必须是 enum 类型
+     * ImmutableBiMap: 不可修改的 BiMap
+     */
+
+    public static void main(String[] args) {
+        BiMap<String,String> weekNameMap = HashBiMap.create();
+        weekNameMap.put("星期一","Monday");
+        weekNameMap.put("星期二","Tuesday");
+        weekNameMap.put("星期三","Wednesday");
+        weekNameMap.put("星期四","Thursday");
+        weekNameMap.put("星期五","Friday");
+        weekNameMap.put("星期六","Saturday");
+        weekNameMap.put("星期日","Sunday");
+
+        System.out.println("星期日的英文名是" + weekNameMap.get("星期日"));
+        System.out.println("Sunday的中文是" + weekNameMap.inverse().get("Sunday"));
+        /**
+         * 星期日的英文名是Sunday
+         * Sunday的中文是星期日
+         */
+    }
+
+}
